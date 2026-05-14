2a8bd52d8d874327871141dbf40995398a0b2f4b
rsthopaz <thopazrosadi15@gmail.com>
2026-05-14 22:54:54 +0700

    add _CA for testing chaincode

--- scripts/env-hq.sh
+++ scripts/env-hq.sh
@@ -13,3 +13,9 @@ export CORE_PEER_TLS_ROOTCERT_FILE=${PWD}/organizations/peerOrganizations/hq.coo
 export CORE_PEER_MSPCONFIGPATH=${PWD}/organizations/peerOrganizations/hq.coop.local/users/Admin@hq.coop.local/msp
 
 export CORE_PEER_ADDRESS=peer0.hq.coop.local:7051
+
+export ORDERER_CA=${PWD}/organizations/ordererOrganizations/orderer.coop.local/tlsca/tlsca.orderer.coop.local-cert.pem
+
+export PEER0_HQ_CA=${PWD}/organizations/peerOrganizations/hq.coop.local/peers/peer0.hq.coop.local/tls/ca.crt
+
+export PEER0_BRANCH_CA=${PWD}/organizations/peerOrganizations/branch.coop.local/peers/peer0.branch.coop.local/tls/ca.crt
